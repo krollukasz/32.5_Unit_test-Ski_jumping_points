680a522c3fef2ddac255484a8f679b0ec2fc7e33
Łukasz Król <lionel@o2.pl>
2019-05-25 19:25:49 +0200

    Add all files with poinst

---
+++ calculateDistancePoints.js
@@ -0,0 +1,19 @@
+const calculateDistancePoints = (distance, hillSize, kPoint) => {
+  // let ponieważ wartość w zmiennej będzie się zmieniać
+  let hillNormal = (distance - kPoint) * 2;
+  let hillBig = (distance - kPoint) * 1.8;
+  let hillMammoth = (distance - kPoint) * 1.2;
+
+  switch (hillSize) {
+    case 'normal':
+      return 60 + hillNormal;
+    case 'big':
+      return 60 + hillBig;
+    case 'mammoth':
+      return 120 + hillMammoth;
+    default:
+      return 'Only three hills available';
+  };
+};
+
+module.exports = calculateDistancePoints;
