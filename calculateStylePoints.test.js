7ee06ee5550dd9e618a08ed9c3f65034859ed4c6
Łukasz Król <lionel@o2.pl>
2019-05-31 10:23:42 +0200

    Fix spelling error, modify script to run all tests

--- calculateStylePoints.test.js
+++ calculateStylePoints.test.js
@@ -3,7 +3,7 @@ const stylePoints = require("./calculateStylePoints");
 
 describe('Style Points', () => {
 
-  it('should return number when notes are the same', () => {
+  it('should return a number when notes are the same.', () => {
     const actual = stylePoints([20, 20, 20, 20, 20]);
 
     const expected = 60;
@@ -11,7 +11,7 @@ describe('Style Points', () => {
     assert.equal(actual, expected);
   });
 
-  it('should return number when two of notes are the same', () => {
+  it('should return a number when two of notes are the same.', () => {
     const actual = stylePoints([17, 17.5, 18, 16.5, 17]);
 
     const expected = 51.5;
@@ -19,7 +19,7 @@ describe('Style Points', () => {
     assert.equal(actual, expected);
   });
 
-  it('should return number when notes are different', () => {
+  it('should return a number when notes are different.', () => {
     const actual = stylePoints([18, 17.5, 16, 16.5, 17]);
 
     const expected = 51;
@@ -27,7 +27,7 @@ describe('Style Points', () => {
     assert.equal(actual, expected);
   });
 
-  it('should return number when two of notes are the same', () => {
+  it('should return a number when two of notes are the same.', () => {
     const actual = stylePoints([9, 9, 8, 8, 8.5]);
 
     const expected = 25.5;
